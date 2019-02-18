283d4e99d1e3c0389f42ca9bfe608804def49e05
duanzhiwei <armchannel_dzw@163.com>
2019-02-18 17:44:37 +0800

    update

--- GCDTimer-demo/ViewController.m
+++ GCDTimer-demo/ViewController.m
@@ -19,29 +19,32 @@
     [super viewDidLoad];
     self.view.backgroundColor =[UIColor colorWithRed:244.0/255.0 green:245.0/255.0 blue:246.0/255.0 alpha:255.0/255.0];
 
-    [self _GCDTimer];
+    [self _GCDTimer3];
 }
 
-
-#pragma mark - GCDTimer
-- (void)_GCDTimer{
+-(void)_GCDTimer1{
     //1.默认使用
-//    self.GCD_Timer = [GCDTimer scheduledTimerWithTimerInterval:1 repeats:YES block:^(dispatch_source_t timer) {
-//        NSLog(@"Hello GCDTimer");
-//    }];
-    
-    //2.在自定义队列中加载timer
-    //    dispatch_queue_t queue = dispatch_queue_create("timerQueue", DISPATCH_QUEUE_CONCURRENT);
-    //    self.GCD_Timer = [GCDTimer timerWithTimeInterval:1 leeway:0 queue:queue repeats:YES block:^(dispatch_source_t timer) {
-    //        NSLog(@"Hello GCDTimer");
-    //    }];
-    
-    //3.使用实例方法初始化timer对象使用
-        self.GCD_Timer = [[GCDTimer alloc]initWithTimerInterval:1 leeway:0 queue:nil repeats:YES block:^(dispatch_source_t timer) {
-            NSLog(@"Hello GCDTimer");
-        }];
+    self.GCD_Timer = [GCDTimer scheduledTimerWithTimerInterval:1 repeats:YES block:^(dispatch_source_t timer) {
+        NSLog(@"Hello GCDTimer");
+    }];
 }
 
+-(void)_GCDTimer2{
+    //2.在自定义队列中加载timer
+    dispatch_queue_t queue = dispatch_queue_create("timerQueue", DISPATCH_QUEUE_CONCURRENT);
+    self.GCD_Timer = [GCDTimer timerWithTimeInterval:1 leeway:0 queue:queue repeats:YES block:^(dispatch_source_t timer) {
+        NSLog(@"Hello GCDTimer");
+        NSLog(@"%@",[NSThread currentThread]);
+    }];
+    
+}
+
+-(void)_GCDTimer3{
+    //3.使用实例方法初始化timer对象使用
+    self.GCD_Timer = [[GCDTimer alloc]initWithTimerInterval:1 leeway:0 queue:nil repeats:YES block:^(dispatch_source_t timer) {
+        NSLog(@"Hello GCDTimer");
+    }];
+}
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [self.GCD_Timer cancel];
